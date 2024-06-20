92f8f31a3dcc3492774b55fc377604ac8296a58e
Marnix Dessing <m.dessing@nijmegen.nl>
2024-06-20 16:35:20 +0200

    feat: remove secure and authorizer from project

--- src/AuthenticationServiceStack.ts
+++ src/AuthenticationServiceStack.ts
@@ -1,15 +1,13 @@
 import { PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
-import { Aspects, Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { IdentitySource, LambdaIntegration, RequestAuthorizer, RestApi, SecurityPolicy } from 'aws-cdk-lib/aws-apigateway';
+import { Aspects, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi, SecurityPolicy } from 'aws-cdk-lib/aws-apigateway';
 import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
 import { ARecord, HostedZone, NsRecord, RecordTarget } from 'aws-cdk-lib/aws-route53';
 import { ApiGateway } from 'aws-cdk-lib/aws-route53-targets';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
-import { JwtAuthorizerFunction } from './authorizer/JwtAuthorizer-function';
 import { Configurable } from './Configuration';
-import { SecureFunction } from './secure/secure-function';
 import { Statics } from './Statics';
 import { TokenFunction } from './token/token-function';
 import { JwksFunction } from './well-known/jwks/jwks-function';
@@ -43,7 +41,6 @@ export class AuthenticationServiceStack extends Stack {
     });
 
     // Example authorization server side (token endpoint, jwks endpoint and openid-configuration endpoint. No authorization endpoint)
-    const secure = api.root.addResource('secure');
     const oauth = api.root.addResource('oauth');
     const wellKnown = oauth.addResource('.well-known');
     const jwksJson = wellKnown.addResource('jwks');
@@ -53,17 +50,6 @@ export class AuthenticationServiceStack extends Stack {
     token.addMethod('POST', new LambdaIntegration(this.tokenEndpoint()));
     jwksJson.addMethod('GET', new LambdaIntegration(this.jwksEndpoint()));
     openidConfiguration.addMethod('GET', new LambdaIntegration(this.openidConfigurationEndpoint()));
-
-    // Example resource server side
-    const jwtAuthorizer = this.requestAuthorizer();
-    const exampleIntegration = new LambdaIntegration(this.secureEndpoint());
-    secure.addMethod('GET', exampleIntegration, {
-      authorizer: jwtAuthorizer,
-
-    });
-    secure.addMethod('POST', exampleIntegration, {
-      authorizer: jwtAuthorizer,
-    });
 
   }
 
@@ -102,28 +88,8 @@ export class AuthenticationServiceStack extends Stack {
     return jwks;
   }
 
-  secureEndpoint() {
-    return new SecureFunction(this, 'secure');
-  }
-
   openidConfigurationEndpoint() {
     return new ConfigFunction(this, 'openid-configuration');
-  }
-
-
-  requestAuthorizer() {
-    return new RequestAuthorizer(this, 'request-authorizer', {
-      handler: new JwtAuthorizerFunction(this, 'jwt-authorizer', {
-        environment: {
-          REQUIRED_SCOPE: 'test',
-          ISSUER: this.subdomain.zoneName,
-        },
-      }),
-      resultsCacheTtl: Duration.minutes(0), // no cashing
-      identitySources: [
-        IdentitySource.header('Authorization'),
-      ],
-    });
   }
 
 
